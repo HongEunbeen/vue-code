939a3e0afbc17cf0e9cf368fd7d78aec2fa0ba1c
HongEunbeen <vvvv980@gmail.com>
2022-07-11 17:08:31 +0900

    chore : project setting

---
+++ vue-guide-project-v3/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+import { onMounted } from 'vue'
+export default{
+  components: {
+
+  },
+  setup(){
+    onMounted(() => {
+      console.log("mounted");
+    })
+  }
+}
+</script>
---
+++ vue-guide-project-v3/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './index.css';
+import {Toast} from 'vue-toastification';
+import "vue-toastification/dist/index.css";
+
+const app = createApp(App);
+const options={};
+
+app.use(Toast, options);
+app.mount('#app');
