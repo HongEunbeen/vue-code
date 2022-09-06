edfa3d63bd48961d0b76c437a44f543b56d70dae
HongEunbeen <vvvv980@gmail.com>
2022-09-07 00:09:39 +0900

    feat: router setup

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  methods:{
-    fetchData(){
-      console.log("hello");
-    }
-  }
+
 }
 </script>
 
