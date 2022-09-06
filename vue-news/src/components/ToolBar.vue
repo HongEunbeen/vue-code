9486b09d685e3a6fcca7d20cbd5cc904e52877dc
HongEunbeen <vvvv980@gmail.com>
2022-09-07 00:19:51 +0900

    router link setup

---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <router-link to="/News">News</router-link> |
+    <router-link to="/Ask">Ask</router-link> |
+    <router-link to="/Jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  color:white;
+  background-color: green;
+  display: flex;
+  padding: 8px;
+}
+.header .router-link-exact-active {
+  color: bule;
+}
+.header a{
+  color: skyblue;
+}
+</style>
